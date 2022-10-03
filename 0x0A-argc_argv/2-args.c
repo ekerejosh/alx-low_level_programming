0b2e91e76c9a6f9622fccf99b4b18ec25ef651a2
ekerejosh <djoshua105@gmail.com>
2022-10-03 00:53:47 -0700

    Task 1

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name number of arguments passed to it
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int counter;
+	for (counter=0; counter<argc; counter ++)
+		printf("argv[%2d]: %s\n",counter,argv[counter]);
+
+	return 0;
+}
+
