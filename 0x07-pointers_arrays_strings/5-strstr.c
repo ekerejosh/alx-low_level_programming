c76cfa19e209dcfa63cf5adb0aec3c9469018cf3
ekerejosh <djoshua105@gmail.com>
2022-10-04 03:56:24 -0700

    Task 5 strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,5 +1,6 @@
-#include "main.h"
 #include <stdio.h>
+/**
+ * main - check the code for AL#include <stdio.h>
 /**
  * main - check the code for ALX students.
  *
@@ -7,11 +8,11 @@
  */
 int main(void)
 {
-	char *s = "world of this";
-	char *f = "world";
-	char *t;
+        char *s = "world of this";
+        char *f = "world";
+        char *t;
 
-	t = _strstr(s, f);
-	printf("%s\n", t);
-	return (0);
+        t = _strstr(s, f);
+        printf("%s\n", t);
+        return (0);
 }
