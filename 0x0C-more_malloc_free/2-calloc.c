2271792122fa6b6f472fcbe6c0504c6028a8c6ed
ekerejosh <djoshua105@gmail.com>
2022-10-10 14:59:14 -0700

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,7 +8,6 @@ include <stdlib.h>
  *
  * Return: void pointer to allocated memory 
  */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *p;
@@ -16,10 +15,14 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
+
 	p = malloc(nmemb * size);
+
 	if (p == NULL)
 		return (NULL);
+
 	for (i = 0; i < (nmemb * size); i++)
 		p[i] = 0;
+
 	return (p);
 }
