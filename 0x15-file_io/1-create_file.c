7d77760bdff9a84d0dd7948d95f6533c8e108684
ekerejosh <djoshua105@gmail.com>
2022-11-05 04:21:49 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,36 +1,51 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
- * read_textfile - reads a text file and prints the letters
- * @filename: filename.
- * @letters: numbers of letters printed.
+ * _strlen - counts string length
+ * @str: string to be used
  *
- * Return: numbers of letters printed. It fails, returns 0.
+ * Return: length of the string
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+int _strlen(char *str)
 {
-	int fd;
-	ssize_t nrd, nwr;
-	char *buf;
+	int len = 0;
 
-	if (!filename)
-		return (0);
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
 
-	fd = open(filename, O_RDONLY);
+/**
+ * create_file -creates a file
+ * @filename: name of the file
+ * @text_content: content of the file to be created
+ *
+ * Return: 1 on success, -1 otherwise
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file, wrote;
 
-	if (fd == -1)
-		return (0);
-
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
-		return (0);
-
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
-
-	close(fd);
-
-	free(buf);
-
-	return (nwr);
+	if (filename == NULL)
+		return (-1);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		wrote = write(file, text_content, _strlen(text_content));
+		if (wrote == -1)
+		{
+			close(file);
+			return (1);
+		}
+		close(file);
+		return (1);
+	}
+	else
+	{
+		close(file);
+		return (1);
+	}
 }
