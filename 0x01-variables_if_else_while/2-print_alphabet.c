96dc5070f398823a285f0913921d044773fead38
ekerejosh <djoshua105@gmail.com>
2023-07-14 14:48:55 -0700

    a script that prints the letters of the alphabets in lowercase using only putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print the letters of the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
