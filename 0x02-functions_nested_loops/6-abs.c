10e3dcfec15106f736c98e577b51da1f59948121
ekerejosh <djoshua105@gmail.com>
2023-07-19 11:10:04 -0700

    This function prints the absolute value of an integer. It returns either - or +

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - Displays the absolute value of an integer
+ * @n: The value of the integer
+ * Return: (0) Success
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
