ec630432b7b29320adec3366cfb95e33b9e99930
ekerejosh <djoshua105@gmail.com>
2023-07-21 04:22:27 -0700

    This program takes in two integers and return the result of the multiplication of the two values

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - Takes two integers and multiplies them
+ * @a: The value of a to be multiplied
+ * @b: The value of b to be multiplied
+ * Return: multiplication.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
