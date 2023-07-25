4d22813495d26ce7f4196e799429a22e69cf33bc
ekerejosh <djoshua105@gmail.com>
2023-07-25 11:50:56 -0700

    This program runs and returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - returns length of the string
+ * @s: input string
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
